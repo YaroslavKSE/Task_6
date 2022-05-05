543186a0797f237de1c241c3eef0423f9a59ec8f
Maxim Pozdniakov <mpozdniakov@kse.org.ua>
2022-05-05 20:18:37 +0300

    Command line

--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
-﻿using System.Diagnostics;
+﻿
+using System.Diagnostics;
 using Task_6;
 
+Point centre = new Point(49.8434957584673, 24.00781313076388);
+double radius = 0.6;
+void Main(string[] args)
+{
+    centre = new Point(Convert.ToDouble(args[0]), Convert.ToDouble(args[1]));
+    radius = Convert.ToDouble(args[2]);
+}
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 var fileReader = new FileReader(new Dictionary<Point, string>(), @"D:\C#\Task_6\ukraine_poi.csv");
 fileReader.ReadFile();
@@ -10,12 +18,13 @@ var root = new Node(new Rectangle(pointMin, pointMax, fileReader.GetData()), nul
 var kdTree = new KdTreeBuilder(root);
 var kd = kdTree.BuildKd(root, fileReader.GetData().Count, 2);
 var rect = new Rectangle(pointMin, pointMax, fileReader.GetData());
-var res = rect.DescribeCircle(new Point(49.8434957584673, 24.00781313076388), 0.6, 45);
-var res2 = rect.DescribeCircle(new Point(49.8434957584673, 24.00781313076388), 0.6, 225);
+var res = rect.DescribeCircle(centre, radius, 45);
+var res2 = rect.DescribeCircle(centre, radius, 225);
 Rectangle describeCircle = new Rectangle(res, res2, new Dictionary<Point, string>());
 Console.WriteLine(rect.DoRectsOverlap(describeCircle));
 KdTreeSearcher toSearch = new KdTreeSearcher(kd, describeCircle);
 var sw = new Stopwatch();
-// fileReader.SearchForPlaces(new Point(49.8434957584673, 49.8434957584673),0.6);
-toSearch.Search(new Point(49.8434957584673, 49.8434957584673), 0.6);
+toSearch.Search(centre, radius);
 Console.WriteLine();
+
+
